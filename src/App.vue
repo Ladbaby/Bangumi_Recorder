7f3ccada0c24b64443fd1b224fca79b0fca840c9
Ladbaby <1390265970@qq.com>
2022-08-15 11:12:08 +0800

    fix:background image in electron

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,176 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from "./components/TodoItem.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    TodoItem,
+  },
+  data() {
+    return {
+      animeList: [
+        { id: 0, text: "Vegetables" },
+        { id: 1, text: "Cheese" },
+        { id: 2, text: "Whatever else humans are supposed to eat" },
+        { id: 3, text: "冰菓" },
+      ],
+      inputText: "Search",
+    };
+  },
+  created() {
+    // this.loadDatabase();
+  },
+  watch: {
+    // animeList: "loadDatabase",
+  },
+  methods: {
+    // async loadDatabase() {
+    //   const fs = require("fs");
+    //   fs.readFile("./database.json", "utf8", (err, jsonObject) => {
+    //     if (err) {
+    //       console.log("Error callback:", err);
+    //       return;
+    //     }
+    //     try {
+    //       console.log(jsonObject); // string object
+    //       const jsonData = JSON.parse(jsonObject);
+    //       console.log(jsonData); //object type
+    //       for (var i = 0; i < jsonData.data.length; ++i) {
+    //         this.animeList.addChild(jsonData.data[i]);
+    //       }
+    //     } catch (err) {
+    //       console.log("Error while parsing JSON string data:", err);
+    //     }
+    //   });
+    // },
+  },
+};
 </script>
+
+<template>
+  <header id="navigator">
+    <!-- <i class="material-icons"> search </i> -->
+    <input
+      autocomplete="off"
+      tabindex="1"
+      placeholder="Search..."
+      type="text"
+      class="search-box-input"
+    />
+  </header>
+  <ol class="card-list">
+    <!--
+      我们给每个 todo 项提供它所表示的 todo 对象，
+      以便能够动态展示内容。
+      同时还需要给每个组件提供一个“key”，
+      这在指南的 v-for 部分有详细解释。
+    -->
+    <TodoItem v-for="item in animeList" :todo="item" :key="item.id"></TodoItem>
+  </ol>
+</template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  background-image: url("./components/image/background.jpg");
+  background-attachment: fixed;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+#navigator {
+  height: 3em;
+  margin-top: 0px;
+  transform: translateY(0px);
+  left: 0px;
+  right: 0px;
+  position: fixed;
+  top: 0;
+  z-index: 5;
+  padding-left: calc(max(env(safe-area-inset-left), 16px)) !important;
+  padding-right: calc(max(env(safe-area-inset-right), 16px)) !important;
+  min-height: calc(56px + env(safe-area-inset-top)) !important;
+  padding-top: env(safe-area-inset-top) !important;
+  transition: all 0.225s cubic-bezier(0.165, 0.84, 0.44, 1);
+  contain: layout;
+  display: block;
+  -webkit-box-flex: 1;
+  flex: 1 1 auto;
+  max-width: 100%;
+  font-family: Roboto, sans-serif;
+  font-size: 16px;
+  overflow-x: hidden;
+  text-rendering: optimizeLegibility;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  -webkit-text-size-adjust: 100%;
+  background-color: hsla(0, 0%, 100%, 0.5) !important;
+  backdrop-filter: blur(5px);
+  color: #000 !important;
+}
+/* div.search-box-div {
+  border-radius: 10px;
+  background-color: hsla(0, 0%, 100%, 0.75) !important;
+  transition: box-shadow 225ms ease-out, -webkit-box-shadow 225ms ease-out;
+  box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.2), 0 5px 8px 0 rgba(0, 0, 0, 0.14),
+    0 1px 14px 0 rgba(0, 0, 0, 0.12) !important;
+  border-color: #fff;
+  color: rgba(0, 0, 0, 0.87);
+  outline: none;
+  text-decoration: none;
+  overflow-wrap: break-word;
+  white-space: normal;
+  will-change: box-shadow;
+  background-repeat: no-repeat;
+  min-height: 3vh;
+  width: 95%;
+  font-family: Roboto, sans-serif;
+  font-size: 16px;
+  overflow-x: hidden;
+  text-rendering: optimizeLegibility;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  -webkit-text-size-adjust: 100%;
+  float: top;
+  display: block;
+  -webkit-transition: width 0.4s ease-in-out;
+  transition: width 0.4s ease-in-out;
+} */
+i.material-icons {
+  vertical-align: middle;
+  left: 0.2em;
+  display: inline-block;
+  margin-right: 0.2em;
+  top: 0.5em;
+}
+input.search-box-input {
+  border-radius: 10px;
+  background-color: hsla(0, 0%, 100%, 0.75) !important;
+  transition: box-shadow 225ms ease-out, -webkit-box-shadow 225ms ease-out;
+  box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.2), 0 5px 8px 0 rgba(0, 0, 0, 0.14),
+    0 1px 14px 0 rgba(0, 0, 0, 0.12) !important;
+  border: 0;
+  outline: 0;
+  color: rgba(0, 0, 0, 0.87);
+  /* background-color: hsla(0, 0%, 100%, 0) !important; */
+  min-height: 1em;
+  height: 2.2em;
+  width: 20%;
+  /* vertical-align: bottom; */
+  left: 0.2em;
+  will-change: box-shadow;
+  font-family: Roboto, sans-serif;
+  font-size: 16px;
+  overflow-x: hidden;
+  position: relative;
+  -webkit-transition: width 0.4s ease-in-out;
+  transition: width 0.4s ease-in-out;
+  display: inline-block;
+  /* align-items: center; */
+  top: 0.5em;
+}
+input.search-box-input:focus {
+  outline: none !important;
+  width: 50%;
+}
+ol.card-list {
+  padding-left: 0;
+  margin-top: 25px;
+  width: 100%;
 }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,92 @@
+<script>
+export default {
+  props: {
+    todo: Object
+  }
+}
+</script>
+
+<template>
+  <span class="card">
+    <a class="cover-div">
+      <img src="./image/98987296_p0.jpg" class="cover-image">
+    </a>
+    <div class="cover-intro">
+      <h2 class="header">{{ todo.text }}</h2>
+    </div>
+  </span>
+</template>
+
+<style scoped>
+.card {
+    border-radius: 10px;
+    background-color: hsla(0,0%,100%,.75)!important;
+    transition: box-shadow 225ms ease-out,-webkit-box-shadow 225ms ease-out;
+    box-shadow: 0 3px 5px -1px rgba(0,0,0,.2),0 5px 8px 0 rgba(0,0,0,.14),0 1px 14px 0 rgba(0,0,0,.12)!important;
+    border-color: #fff;
+    color: rgba(0,0,0,.87);
+    outline: none;
+    text-decoration: none;
+    overflow-wrap: break-word;
+    position: relative;
+    white-space: normal;
+    will-change: box-shadow;
+    background-repeat: no-repeat;
+    min-height: 20vh;
+    width: 47%;
+    /* max-width: 200%; */
+    font-family: Roboto,sans-serif;
+    font-size: 16px;
+    overflow-x: hidden;
+    text-rendering: optimizeLegibility;
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+    -webkit-text-size-adjust: 100%;
+    margin-top: 2vh;
+    margin-right: 2vh;
+    display: inline-block;
+}
+
+.cover-div {
+    /* display: inline-block; */
+    width: 130px;
+    height: 230px;
+    overflow: hidden;
+    margin: 10px;
+    position: relative;
+    border-radius: 10px;
+    float:left;
+}
+
+.cover-image {
+    position: absolute;
+    left: -1000%;
+    right: -1000%;
+    top: -1000%;
+    bottom: -1000%;
+    margin: auto;
+    min-height: 100%;
+    min-width: 100%;
+    height:100%;
+    width:auto;
+}
+
+.cover-intro {
+    width: 100%;
+    padding-left: 20px;
+    vertical-align: top;
+    margin: 0;
+    padding: 0;
+    font-weight: 400;
+    box-sizing: border-box;
+    font-family: Microsoft YaHei,微软雅黑,STHeiti,WenQuanYi Micro Hei,SimSun,sans-serif;
+    font-size: 14px;
+    color: #666;
+    position: relative;
+}
+
+.header {
+    font-size: 20px;
+    /* float:right; */
+}
+
+</style>
